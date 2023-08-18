f9fb1aa9038271a7d9bc91056b35aa6f83a12542
JosephAwuku33 <33jaykay@gmail.com>
2023-08-18 10:47:41 +0000

    setting up data models

---
+++ lib/models/habit.dart
@@ -0,0 +1,12 @@
+class Habit {
+  final String title;
+  final String details;
+  final String activity;
+  final String period;
+
+  Habit(
+      {required this.title,
+      required this.details,
+      required this.activity,
+      required this.period});
+}
