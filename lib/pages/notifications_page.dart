e14daf1598bd098c0e37b194da90586feed78c10
JosephAwuku33 <33jaykay@gmail.com>
2023-08-28 14:51:26 +0000

    setting up notification handler

--- lib/pages/notifications_page.dart
+++ lib/pages/notifications_page.dart
@@ -5,6 +5,8 @@ class NotificationsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("Notifications"));
+    return const SingleChildScrollView(
+      child: Column(),
+    );
   }
 }
